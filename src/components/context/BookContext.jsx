05b129b3087f79b9a6fb0999576c4a3e262c396a
Francisco Correia28 <francisco.correia1804@gmail.com>
2020-09-27 15:25:15 +0100

    adicionando e carregando items do localStorage

--- src/components/context/BookContext.jsx
+++ src/components/context/BookContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useEffect } from 'react';
 import { bookReducer } from '../reducers/BookReducer';
 export const BookContext = createContext()
 export default function BookContextProvider(props) {
@@ -6,10 +6,15 @@ export default function BookContextProvider(props) {
     //     { title: 'O Milagre da Manhã', author: 'desconhecido', id: 1 },
     //     { title: 'Seja a tua melhor versão', author: 'desconhecido', id: 2 }
     // ])
-    const [books, dispatch] = useReducer(bookReducer, [])
+    const [books, dispatch] = useReducer(bookReducer, [], () => {
+        const localData = localStorage.getItem('books');
+        return localData ? JSON.parse(localData) : [];
+    })
     // const addBook = (title, author) => { setBooks([...books, { title, author, id: uuid() }]) }
     // const removeBook = (id) => { setBooks(books.filter(book => book.id !== id)) }
-
+    useEffect(() => {
+        localStorage.setItem('books', JSON.stringify(books))
+    }, [books]);
     return (
         <BookContext.Provider value={{ books, dispatch }}>
             {props.children}
