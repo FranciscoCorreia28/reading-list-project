c1657eb8c0db1072f7ceb7f2fda8f3e3b2abb964
Francisco Correia28 <francisco.correia1804@gmail.com>
2020-09-27 13:41:48 +0100

    Navbar e listagem de livros concluidos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,16 @@
 import React, { useEffect } from 'react';
-import Loader from './loader/Loader.jsx';
-import MainHeader from './header/MainHeader';
+import Navbar from './Navbar';
+import BookContextProvider from './context/BookContext';
+import '../assets/css/style.css'
 
-
-/********IMPORTANDO OS ESTILOS***********/
-import '../assets/css/bootstrap.min.css';
-import '../assets/css/style.css';
-import '../assets/css/font-awesome.min.css';
-import '../assets/css/elegant-icons.css';
-import '../assets/css/nice-select.css';
-/***************************************/
 
 export default function () {
-    const useEffect = () => {
-        setTimeout(() => {
-            return <div> <Loader /></div>
-        }, 2000);
-        return () => {
-            const valor = { display: "none" };
-            return <div> <Loader valor={valor} /></div>
-        }
-    }
+
     return (
-        <div>
-            <MainHeader />
+        <div className="App">
+            <BookContextProvider>
+                <Navbar />
+            </BookContextProvider>
         </div>
     );
 }
---
+++ src/components/context/BookContext.jsx
@@ -0,0 +1,18 @@
+import React, { createContext, useState } from 'react';
+import { v4 as uuid } from 'uuid'
+export const BookContext = createContext()
+export default function BookContextProvider(props) {
+    const [books, setBooks] = useState([
+        { title: 'O Milagre da Manhã', author: 'desconhecido', id: 1 },
+        { title: 'Seja a tua melhor versão', author: 'desconhecido', id: 2 }
+    ])
+
+    const addBook = (title, author) => { setBooks([...books, { title, author, id: uuid() }]) }
+    const removeBook = (id) => { books.filter(book => book.id !== id) }
+
+    return (
+        <BookContext.Provider value={{ books, addBook, removeBook }}>
+            {props.children}
+        </BookContext.Provider>
+    );
+};
